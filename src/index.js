179a993aae3ac9e3ecf6bbe8bbea0b511cd9fad0
Tom Smalley <tfsmalley@gmail.com>
2020-09-04 13:48:52 +0100

    More transport testing

--- src/index.js
+++ src/index.js
@@ -1,33 +1,55 @@
-import _ from 'lodash';
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
+import TransportWebHID from "@ledgerhq/hw-transport-webhid";
+import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
 import Ledger from "@ledgerhq/hw-app-avalanche";
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+async function test_ledger(transport) {
+  console.log('Creating ledger...');
+  const ledger = new Ledger(transport);
+  console.log('Getting wallet ID...');
+  const device_wallet_id = await ledger.getWalletId().catch(console.error);
+  console.log(device_wallet_id.toString("hex"));
+  console.log('done');
 }
 
-document.body.appendChild(component());
-
 async function test_u2f() {
-  var U2FSupported = await TransportU2F.isSupported();
+  const U2FSupported = await TransportU2F.isSupported();
   if (!U2FSupported) {
     console.log("U2F is not supported");
     return;
   }
   console.log('U2F is supported');
-  console.log('Creating transport...');
   const transport = await TransportU2F.create();
-  console.log('Creating ledger...');
-  const ledger = new Ledger(transport);
-  console.log('Getting wallet ID...');
-  const device_wallet_id = await ledger.getWalletId().catch(e => console.error("[Skipped: Couldn't get wallet ID]:", e, e.originalError));
-  console.log(device_wallet_id.toString("hex"));
-  console.log('done');
+  await test_ledger(transport);
 }
 
-test_u2f()
+async function test_hid() {
+  const WebHIDSupported = await TransportWebHID.isSupported();
+  if (!WebHIDSupported) {
+    console.log("WebHID is not supported");
+    return;
+  }
+  console.log('WebHID is supported');
+  const transport = await TransportWebHID.create();
+  await test_ledger(transport);
+}
+
+async function test_usb() {
+  const WebUSBSupported = await TransportWebUSB.isSupported();
+  if (!WebUSBSupported) {
+    console.log("WebUSB is not supported");
+    return;
+  }
+  console.log('WebUSB is supported');
+  const transport = await TransportWebUSB.create().catch(console.error);
+  if (transport === undefined) {
+    console.log("Failed to create transport");
+    return;
+  }
+  await test_ledger(transport);
+}
+
+document.querySelector('#u2f').onclick = test_u2f;
+document.querySelector('#hid').onclick = test_hid;
+document.querySelector('#usb').onclick = test_usb;
+
